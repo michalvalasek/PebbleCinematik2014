d0cc19cdcb24c908f060636a44bd388179af2c42
Michal Valasek <michalvalasek@users.noreply.github.com>
2014-09-07 23:04:21 +0200

    Version 1.1
    Event details are not being loaded individually.
    Removed refresh by shake.
    Improved splash screen with Cinematik logo.

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 var ajax = require('ajax');
-var Accel = require('ui/accel');
-var Vibe = require('ui/vibe');
+//var Accel = require('ui/accel');
+//var Vibe = require('ui/vibe');
 
 var API_URL = 'http://cinematik-2014.herokuapp.com';
+var EVENTS_LIMIT = 10;
 
 // Show splash screen while waiting for data
 var splashWindow = new UI.Window({
@@ -18,24 +19,35 @@ var splashWindow = new UI.Window({
   color: 'white'
 });
 
-// Text element to inform user
-var text = new UI.Text({
-  position: new Vector2(0, 30),
-  size: new Vector2(144, 40),
-  text: "CINEMATIK 2014\nLoading...",
+var logo = new UI.Image({
+  position: new Vector2(0,30),
+  size: new Vector2(144,40),
+  image: 'images/cinematik_logo.png'
+});
+var text_2014 = new UI.Text({
+  position: new Vector2(0, 65),
+  size: new Vector2(144, 30),
+  text: "2014",
   font: 'gothic-28-bold',
   color: 'white',
-  textOverflow: 'wrap',
   textAlign: 'center'
 });
-
-// Add to splashWindow and show
-splashWindow.add(text);
+var text_loading = new UI.Text({
+  position: new Vector2(0, 100),
+  size: new Vector2(144, 20),
+  text: "Nacitava sa...",
+  font: 'gothic-18',
+  color: 'white',
+  textAlign: 'center'
+});
+splashWindow.add(logo);
+splashWindow.add(text_2014);
+splashWindow.add(text_loading);
 splashWindow.show();
 
-function parseEvents(data) {
+function buildEventsMenuItems(events) {
   var menu_items = [];
-  data.forEach(function(event){
+  events.forEach(function(event){
     menu_items.push({
       id: event.id,
       title: event.time + ', ' + event.place,
@@ -45,55 +57,58 @@ function parseEvents(data) {
   return menu_items;
 }
 
-// Load list of 5 upcomming events
-ajax({ url: API_URL+'/events/upcoming/5/thumbnails', type: 'json' },
+var events_data = [];
+
+// Load list of upcomming events
+ajax({ url: API_URL+'/events/upcoming/'+EVENTS_LIMIT, type: 'json' },
   function(data){
+    events_data = data;
     var eventsMenu = new UI.Menu({
       sections: [{
-        title: 'Upcoming events',
-        items: parseEvents(data)
+        title: 'Najblizsie predstavenia',
+        items: buildEventsMenuItems(events_data)
       }]
     });
+    
+    var detailCard = new UI.Card({
+      title: 'Nacitava sa...',
+      style: 'small',
+      scrollable: true
+    });
+    
     eventsMenu.on('select', function(e) {
-      var detailCard = new UI.Card({
-        title: 'Loading...',
-        style: 'small',
-        scrollable: true
-      });
       detailCard.show();
       
-      ajax({url: API_URL+'/events/'+e.item.id, type: 'json'},
-        function(data){  
-          var content = data.time + ', ' + data.place + 
-              "\n" + data.original_name + 
-              "\nSekcia: " + data.section + 
-              "\nRezia: " + data.director + 
-              "\n" + data.meta;
-          detailCard.title('');
-          detailCard.subtitle(data.name);
-          detailCard.body(content);
-        },
-        function(error) {
-          console.log("Event loading failed: " + error);
-        });
+      var current_event = events_data.filter(function(evt){ return evt.id == e.item.id; })[0];
+      var content = current_event.time + ', ' + current_event.place + 
+          "\n" + current_event.original_name + 
+          "\nSekcia: " + current_event.section + 
+          "\nRezia: " + current_event.director + 
+          "\n" + current_event.meta;
+      detailCard.title('');
+      detailCard.subtitle(current_event.name);
+      detailCard.body(content);
     });
+    
     eventsMenu.show();
     splashWindow.hide(); // Hide the splashWindow to avoid showing it when the user press Back.
     
     // Refresh events list on shake
-    eventsMenu.on('accelTap', function(e) {
+    /*eventsMenu.on('accelTap', function(e) {
       ajax(
-        { url: API_URL+'/events/upcoming/5/thumbnails', type:'json' },
+        { url: API_URL+'/events/upcoming/'+EVENTS_LIMIT, type:'json' },
         function(data) {
-          // Update the Menu's first section
-          eventsMenu.items(0, parseEvents(data));
+          events_data = data;
+          // Update the Menu's first section and show it
+          eventsMenu.items(0, buildEventsMenuItems(events_data));
+          detailsCard.hide();
           // Notify the user
           Vibe.vibrate('short');
         },
         function(error) {
           console.log('Refresh failed: ' + error);
         });
-    });
+    });*/
   },
   function(error) {
     console.log("Events list loading failed: " + error);
@@ -101,4 +116,4 @@ ajax({ url: API_URL+'/events/upcoming/5/thumbnails', type: 'json' },
 );
 
 // Prepare the accelerometer
-Accel.init();
+//Accel.init();
